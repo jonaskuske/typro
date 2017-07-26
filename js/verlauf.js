26272d14f52dd83e8e904dd2cf63cd9fd5b97ad4
Jonas <30421456+jonaskuske@users.noreply.github.com>
2017-07-26 13:43:52 +0200

    JS update (see stable), potential getUserMedia improvements

--- js/verlauf.js
+++ js/verlauf.js
@@ -1,18 +1,16 @@
 // Auslesen der Bilder aus dem Local Storage und Anzeigen auf Verlauf-Seite
-$(document).on("pageshow", "#verlauf", function(){
-    "use strict";
-	// Prüfen ob Eintrag in lStorage vorhanden, wenn nicht: Erstellen
-    if (localStorage.getItem("picArray") === null) {
-        picArray=[];
-        localStorage.setItem("picArray", JSON.stringify(picArray));
-    }
-    var verlaufArray;
-    var verlaufArrayL;
-	// Array aus lStorage abrufen & Länge ermitteln
-    verlaufArray = JSON.parse(localStorage.getItem("picArray"));
-    verlaufArrayL = verlaufArray.length;
-	// Daten aus Array darstellen
-    for ( var i=0; i<verlaufArrayL; i++ ) {
-        $("#content_verlauf").append('<img class="verlaufPic" src ='+verlaufArray[i]+'>');
-    }
+$(document).on("pageshow", "#verlauf", function () {
+	"use strict";
+	// Falls Bilder in lStorage: Anzeigen
+	if (localStorage.getItem("picArray") !== null) {
+		var verlaufArray;
+		var verlaufArrayL;
+		// Array aus lStorage abrufen & Länge ermitteln
+		verlaufArray = JSON.parse(localStorage.getItem("picArray"));
+		verlaufArrayL = verlaufArray.length;
+		// Daten aus Array darstellen
+		for (var i = 0; i < verlaufArrayL; i++) {
+			$("#content_verlauf").append('<img class="verlaufPic" src =' + verlaufArray[i] + '>');
+		}
+	}
 });
